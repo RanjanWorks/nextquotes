68e6d72e136e1103db5fd68abf78a91ab2b6869a
Ranjan kashyap <kashyapranjan9977@gmail.com>
2024-04-20 16:52:24 +0530

    favicon

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import Tooltip from '@mui/material/Tooltip';
